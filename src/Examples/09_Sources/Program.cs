252e7dfd4b73369a1f49eaef3975075061edaa85
luni64 <lutz@niggl-schlagbauer.de>
2024-06-09 08:56:43 +0200

    Added source example

---
+++ src/Examples/09_Sources/Program.cs
@@ -0,0 +1,40 @@
+﻿using rmSharp;
+using System.Diagnostics;
+
+DB.sqLiteFile = "someDatabase.rmtree";
+
+using (var db = new DB())
+{
+
+    // print all citations
+    foreach (var repository in db.Repositories)
+    {
+        Trace.WriteLine(repository);
+        foreach (var source in repository.Sources)
+        {
+            Trace.WriteLine($"   -{source}");
+            foreach (var citation in source.Citations)
+            {
+                Trace.WriteLine($"    -{citation}");
+            }
+        }
+    }
+
+    // find all persons with more than 3 events and print corresponding citations
+    var p = db.Persons.Where(p => p.Events.Count() > 3).FirstOrDefault();
+    Trace.WriteLine($"{p}");
+    foreach(var ev in p.Events.Where(e=>e.Citations.Count > 0))    
+    {
+        Trace.WriteLine($"  {ev}");
+        foreach(var citation in ev.Citations)
+        {
+            Trace.WriteLine($"    Citation:  {citation} Source:{citation.Source}");
+        }
+    }
+}
+
+
+
+
+
+
